f643d4f5700c82a3479d17b27bef743deadd4a99
Mateusz Skrzat <mateusz.skrzat1996@gmail.com>
2018-04-19 16:04:25 +0200

    Problem solved by my formula

---
+++ UrodzinyJasia/OstatniaOsoba.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UrodzinyJasia
+{
+    class OstatniaSwieczka
+    {
+        long l, z, z1; //l = ilosc świeczek, z = cyfra 2^n, z1 = zmienna pomocnicza, zapisująca 2^n-1
+        int n; // potęgi liczby 2
+        public OstatniaSwieczka() {
+            Console.WriteLine("Podaj ile świeczek ma zdmuchnąć Jaś");
+            l = int.Parse(Console.ReadLine()); //wczytanie ilosci świeczek
+            z = 2;//z = 2^1
+            n = 2;
+            WypiszOstatniaSwieczke();
+        }
+        void WypiszOstatniaSwieczke() {
+            while (true)
+            {
+                if (l > z + 1)//if ilość świeczek > (2^n) + 1 => z = z + 2^(n)
+                {
+                    z1 = z; //zapis z do zmiennej z1
+                    z = z + (long)Math.Pow(2, n); //dodawanie kolejnej potegi
+                    n++; //zwiekszanie potegi o 1
+                }
+                else if (l == z + 1)//jesli zmienna l jest rowna spotegowanemu (2^n) + 1, ostatnia zapalona świeczka to (2^n) + 1
+                { 
+                    Console.WriteLine("Ostatnia zapalona świeczka to " + (z + 1));
+                    break;
+                }
+                else //jesli liczba świeczek jest mniejsza od z+1
+                { 
+                    Console.WriteLine("Ostatnia zapalona świeczka to " + ((((z1 + l + 1) % (z + 1)) * 2) + 1));//((((2^(n-1) + ilość świeczek + 1) mod ((2^n) + 1)) * 2) + 1)
+                    //pobieramy z1 jako poprzednio spotegowana z, dodajemy liczbe osob i cyfre 1, obliczamy reszte z dzielenia sumy przez z + 1
+                    //wynik ten mnozymy razy 2, i na koniec dodajemy 1, wynikiem jest ostatnia zapalona świeczka
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ UrodzinyJasia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UrodzinyJasia
+{
+    //TREŚĆ ZADANIA
+    //Kiedy Jaś obchodzi swoje urodziny, dostaje zawsze tort ze świeczkami, które są ponumerowane i uporządkowane rosnąco po okręgu.
+    //Jaś zaczynając od pierwszej świeczki, zdmuchuje co drugą niezgaszoną świeczkę (pierwsza zdmuchnięta świeczka to nr 2) tak długo,
+    //aż pozostanie tylko jedna. Jasia przed rytuałem zdmuchiwania zawsze zastanawia, która świeczka pozostanie niezgaszona.
+    //Pomóż Jasiowi odpowiedzieć na to pytanie, wiedząc, które urodziny obchodzi
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                OstatniaSwieczka ostatnia = new OstatniaSwieczka();
+                Console.ReadKey();
+            }
+        }
+    }
+}
